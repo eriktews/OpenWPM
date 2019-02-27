6ff126036eb492b760f74d79334c8366c94c3a68
Erik Tews <erik@datenzone.de>
2019-02-27 22:02:36 +0100

    Fix the build by downloading and building both npm packages locally

--- build-extension.sh
+++ build-extension.sh
@@ -2,11 +2,23 @@
 set -e
 
 rm -rf automation/Extension/firefox
-git clone https://github.com/nhnt11/openwpm-firefox-webext automation/Extension/firefox
-cd automation/Extension/firefox
-npm install
-ls -la node_modules
-#npm install
 
+# For some reasons, the openwpm-firefox-webext package fails to build
+# when the openwpm-webext-instrumentation is referenced via github.
+#
+# We just clone and build the repository locally then.
+#
+# Change this back to the original behaviour once the problem has been
+# fixed in upstream.
+
+rm -rf automation/Extension/openwpm-webext-instrumentation
+git clone https://github.com/eriktews/openwpm-firefox-webext.git automation/Extension/firefox
+git clone https://github.com/mozilla/openwpm-webext-instrumentation.git automation/Extension/openwpm-webext-instrumentation
+cd automation/Extension/openwpm-webext-instrumentation
+npm install
+npm run build
+cd ../firefox
+git checkout local-build
+npm install
 npm run build
 cp dist/*.zip ./openwpm.xpi
